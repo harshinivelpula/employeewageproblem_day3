e29520344a1ed36c9b816aec3da6d7d0f69cedd8
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-14 15:34:58 +0530

    check attendance

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿namespace EMPLOYEE213
+﻿namespace employeewage_day3
 {
     class program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to employee wage problem");
+            employeecheck.CheckAttendance();
         }
     }
 }

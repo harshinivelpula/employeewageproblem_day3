cacea46694cc0aa08f99d23e925ec22aa47a5232
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-14 16:42:24 +0530

    cal parttime

---
+++ parttime.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace employeewage_day3
+{
+    public class parttime
+    {
+        public static void isparttime()
+        {
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(3);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("employee is full time");
+                emphrs = 8;
+            }
+            else if(empcheck == IS_PART_TIME)
+            {
+                Console.WriteLine("employee is part time");
+                emphrs = 4;
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+                emphrs = 0;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp wage:" + empwage);
+        }
+    }
+}
+

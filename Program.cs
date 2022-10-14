357fe84c91919d9b76bee2851a7b3d299ce68272
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-14 14:59:23 +0530

    greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EMPLOYEE213
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage problem");
+        }
+    }
+}
